771d4e5e494ad4f0520b47867df84922d6be46cb
nehamehandole2001@gmail.com <neha@leocoders.com>
2024-07-08 16:34:56 +0530

    update

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -6,7 +6,7 @@ import About from '../about/About'
 import Projects from '../projects/Projects';
 import Experience from '../experience/Experience';
 import Contact from '../contact/Contact';
-import Nehacv from '../../assets/Neha-rs Resume.pdf'
+import Nehacv from '../../assets/Nehar-resume.pdf'
 
 
 const Home = () => {
